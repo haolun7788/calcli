973a176a24b826f1582a33b96b545c0d803d3cae
crispo513 <haolun7788@gmail.com>
2026-09-18 11:30:14 -0700

    Add CLI11 dependency + add subcommand stub

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Simple C++ CLI for quick Google Calender entries. Work in Progress.
+
+cmake --build build
+
+./build/Debug/calcli
+
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+#include <CLI/CLI.hpp>
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "calcli: build works" << std::endl;
+int main(int argc, char **argv) {
+    CLI::App app{"calcli - fast Google Calender management"};
+    app.require_subcommand(1);
+
+    std::string title;
+    std::string due;
+    
+    auto* add_cmd = app.add_subcommand("add", "Add new event");
+    add_cmd->add_option("title", title, "Event title")->required();
+    add_cmd->add_option("--due", due, "Due date/time (natural language)")->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    if (*add_cmd) {
+        std::cout << "Would add event: \"" << title
+                    << "\" due \"" << due << "\"\n";
+    }
+
     return 0;
 }
